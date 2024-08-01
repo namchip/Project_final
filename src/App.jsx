1125b4a599cf7a068e7e4590518e7e640a9444a3
Pearn203 <kog151603@gmail.com>
2024-08-01 20:55:15 +0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Sidebar from './components/Sidebar';
+import './css/Sidebar.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      {/* Các thành phần khác của ứng dụng */}
+    </div>
+  );
+}
+
+export default App;
+
